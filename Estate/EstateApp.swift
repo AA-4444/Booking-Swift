21262eab8e05efc0626d9558f5c0abbf102d788d
Alex <104495043+AA-4444@users.noreply.github.com>
2025-03-14 13:36:17 +0100

    Initial commit

---
+++ Estate/EstateApp.swift
@@ -0,0 +1,17 @@
+//
+//  EstateApp.swift
+//  Estate
+//
+//  Created by Алексей Зарицький on 3/14/25.
+//
+
+import SwiftUI
+
+@main
+struct EstateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
